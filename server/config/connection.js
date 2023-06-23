d5a49594615d0c6db7ece38e091050665e058c9a
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-06-23 08:27:07 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,5 +4,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookSearc
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+  
 module.exports = mongoose.connection;
